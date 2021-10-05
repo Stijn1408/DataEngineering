588d55dcdc132a199d7160a8b5b108e120e9b7eb
Stijn1408 <s.biemans2000@gmail.com>
2021-10-05 11:18:55 +0200

    Push

--- visualization-cp/resources/visualization.py
+++ visualization-cp/resources/visualization.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 def score_bar_plot(score):
-    fig = plt.figure()
-    ax = fig.add_axes([0,0,1,1])
-    ax.bar(score.keys(), score.values())
+    # fig = plt.figure()
+    # ax = fig.add_axes([0,0,1,1])
+    # ax.bar(score.keys(), score.values())
     #plt.show()
     print("A plot was created :-)")
