aa4755574860de1379e6b35e8503ace055f8a60b
Stijn1408 <s.biemans2000@gmail.com>
2021-10-05 15:25:05 +0200

    Push

--- preprocessing-cp/app.py
+++ preprocessing-cp/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, json, request, Response, jsonify, send_file
-
-from resources import preproces
+import os
+import pandas as pd
+from resources import preprocess
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/preprocessing-cp/results', methods=['POST'])
 def df_preprocessing():
-    content = request.get_json()
-    df = pd.read_json(json.dumps(content), orient='records')
-    scaled_df = preproces.preprocessing(df)
+    # db_api = os.environ['TRAININGDB_API']
+    # content = request.get(db_api)
+    content  = request.get_json()
+    #df = pd.read_json(json.dumps(content), orient='records')
+    #j = content.json()
+    df = pd.DataFrame.from_dict(content)
+    scaled_df = preprocess.preprocessing(df)
     return scaled_df
 
 app.run(host='0.0.0.0', port=5000)
