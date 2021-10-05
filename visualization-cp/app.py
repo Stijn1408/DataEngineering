c22cf06d7e72f6c292da9153a108bf1b38f19517
Stijn1408 <s.biemans2000@gmail.com>
2021-10-05 12:37:23 +0200

    Push

--- visualization-cp/app.py
+++ visualization-cp/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, json, request, Response, jsonify
-import matplotlib.pyplot as plt
+from flask import Flask, json, request, Response, jsonify, send_file
 
 from resources import visualization
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config["DEBUG"] = True
 
 
@@ -12,7 +11,26 @@ def bar_plot():
     # receive the prediction request data as the message body
     content = request.get_json()
     scores = json.loads(content)
-    plot = visualization.score_bar_plot(scores)
-    return plot
+    bytes_object = visualization.score_bar_plot(scores)
+    return send_file(bytes_object, attachment_filename = 'barchart.png', mimetype = 'image/npg')
 
 app.run(host='0.0.0.0', port=5000)
+
+# from flask import Flask, json, request, Response, jsonify
+# import matplotlib.pyplot as plt
+#
+# from resources import visualization
+#
+# app = Flask(__name__)
+# app.config["DEBUG"] = True
+#
+#
+# @app.route('/visualization-cp/results', methods=['POST'])
+# def bar_plot():
+#     # receive the prediction request data as the message body
+#     content = request.get_json()
+#     scores = json.dumps(content)
+#     plot = visualization.score_bar_plot(scores)
+#     return plot
+#
+# app.run(host='0.0.0.0', port=5000)
--- visualization-cp/resources/visualization.py
+++ visualization-cp/resources/visualization.py
@@ -1,15 +1,26 @@
 import matplotlib.pyplot as plt
-from flask import jsonify
-
+import io
 
 def score_bar_plot(score):
-    # fig = plt.figure()
-    # ax = fig.add_axes([0,0,1,1])
-    # ax.bar(score.keys(), score.values())
-    #plt.show()
-    key = score.keys()
-    text_out = {
-        "Message:": "A plot was created :-)",
-        "Keys:": key
-    }
-    return jsonify(text_out), 200
+    f, ax = plt.subplots(figsize=(11, 9))
+    plt.bar(score.keys(), score.values())
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    bytes_image.seek(0)
+    return bytes_image
+
+# import matplotlib.pyplot as plt
+# from flask import jsonify
+#
+#
+# def score_bar_plot(score):
+#     # fig = plt.figure()
+#     # ax = fig.add_axes([0,0,1,1])
+#     # ax.bar(score.keys(), score.values())
+#     #plt.show()
+#     key = score.keys()
+#     text_out = {
+#         "Message:": "A plot was created :-)",
+#         "Keys:": key
+#     }
+#     return jsonify(text_out), 200
