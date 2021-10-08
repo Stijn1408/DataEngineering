097890e48e3664eef9d7b4426adef77a6d367fd9
Stijn1408 <s.biemans2000@gmail.com>
2021-10-08 13:52:55 +0200

    Push

--- sampleprediction-cp/app.py
+++ sampleprediction-cp/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, json, request, Response, jsonify, send_file
 
 from resources import sampleprediction
+import ast
+import requests
 import pandas as pd
-
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/sample-cp/results', methods=['POST'])
 def sample_plot():
-    """Displays a html table of a random sample of all predictions"""
-    content = request.get_json()
+    # receive the prediction request data as the message body
+    db_api = os.environ['PREDICTION_API']
+    content = requests.get(db_api)
+    content = content.json()
     df = pd.read_json(json.dumps(content), orient='records')
     table = sampleprediction.sample_plot(df)
     return table.to_html(header = True, table_id = "Random Prediction Sample")
