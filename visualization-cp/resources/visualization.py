cd830dfaa2064014cf95c97caa1324c6ffc51bc3
max1999mae <maxkanters@hotmail.nl>
2021-10-08 13:59:26 +0200

    Push

--- visualization-cp/resources/visualization.py
+++ visualization-cp/resources/visualization.py
@@ -5,6 +5,9 @@ def score_bar_plot(score):
     """Displays a barchart of the model scores"""
     f, ax = plt.subplots(figsize=(11, 9))
     plt.bar(score.keys(), score.values())
+    plt.xlabel("Metric")
+    plt.ylabel("Score")
+    plt.title("Model scores")
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
