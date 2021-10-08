3240544782555492a41651900253592bf31fa211
max1999mae <maxkanters@hotmail.nl>
2021-10-08 13:49:50 +0200

    Push

--- sampleprediction-cp/app.py
+++ sampleprediction-cp/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, json, request, Response, jsonify, send_file
 
 from resources import sampleprediction
-import ast
 import pandas as pd
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/sample-cp/results', methods=['POST'])
 def sample_plot():
-    # receive the prediction request data as the message body
-
+    """Displays a html table of a random sample of all predictions"""
     content = request.get_json()
     df = pd.read_json(json.dumps(content), orient='records')
     table = sampleprediction.sample_plot(df)
